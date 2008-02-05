6054cce1fca6fcac52176a8acf5d2e376ac11430
Ricardo SIGNES <rjbs@cpan.org>
2008-02-04 22:09:37 -0500

    my bundle

---
+++ lib/Task/BeLike/RJBS.pm
@@ -0,0 +1,57 @@
+
+use strict;
+use warnings;
+package Task::BeLike::RJBS;
+
+=head1 NAME
+
+Task::BeLike::RJBS - be more like RJBS -- use the modules he likes!
+
+=head1 DESCRIPTION
+
+This L<Task|Task> module installs the modules that I, RJBS, really need to get
+work done.
+
+Here are the contents:
+
+  requires('App::Ack',                       '1.76'  );
+  requires('App::Addex',                     '0.013' );
+  requires('App::Addex::AddressBook::Apple', '0.006' );
+  requires('App::Cmd',                       '0.012' );
+  requires('CPAN::Mini',                     '0.566' );
+  requires('CPAN::Reporter',                 '1.0602');
+  requires('Config::INI',                    '0.011' );
+  requires('Data::GUID',                     '0.043' );
+  requires('DateTime',                       '0.41'  );
+  requires('Email::Filter',                  '1.031' );
+  requires('Email::MIME',                    '1.861' );
+  requires('Exception::Class',               '1.23'  );
+  requires('File::Next',                     '1.02'  );
+  requires('File::Temp',                     '0.20'  );
+  requires('File::Tempdir',                  '0.02'  );
+  requires('HTML::Element',                  '3.23'  );
+  requires('LWP',                            '5.808' );
+  requires('List::MoreUtils',                '0.21'  );
+  requires('Mac::Glue',                      '1.30'  );
+  requires('Mixin::ExtraFields',             '0.007' );
+  requires('Mixin::ExtraFields::Param',      '0.011' );
+  requires('Module::Install',                '0.68'  );
+  requires('Moose',                          '0.35'  );
+  requires('MooseX::POE',                    '0.07'  );
+  requires('MooseX::Singleton',              '0.05'  );
+  requires('PPI',                            '1.201' );
+  requires('Params::Util',                   '0.31'  );
+  requires('Perl::Critic',                   '1.080' );
+  requires('Perl::Critic::Lax',              '0.007' );
+  requires('Rubric',                         '0.142' );
+  requires('Scalar::Util',                   '1.19'  );
+  requires('Slash::Client::Journal',         '0.01'  );
+  requires('Sub::Exporter',                  '0.978' );
+  requires('Text::Markdown',                 '1.0.5' );
+  requires('Tie::CPHash',                    '1.03'  );
+  requires('YAML::Syck',                     '1.01'  );
+  requires('pip',                            '0.13'  );
+
+=cut
+
+1;
