802c24fe4cdb1f9b801d0931734e7c8278510bf1
Ricardo Signes <rjbs@cpan.org>
2009-11-29 10:49:22 -0500

    include more, reorder listings

--- lib/Task/BeLike/RJBS.pm
+++ lib/Task/BeLike/RJBS.pm
@@ -4,11 +4,6 @@ use warnings;
 package Task::BeLike::RJBS;
 # ABSTRACT: be more like RJBS -- use the modules he likes!
 
-=head1 DESCRIPTION
-
-This L<Task|Task> module installs the modules that I, RJBS, really need to get
-work done.
-
 =pkgroup Useful Command-Line Tools
 
 =pkg App::Ack 1.76 fixes for perl 5.10
@@ -19,11 +14,9 @@ App::Ack also gets us File::Next!
 
 =pkg App::Whiff
 
-=pkgroup everything else
 
-=pkg App::Cmd 0.303 App::Cmd::Simple; compat with Getopt::Long::Descripive 0.081
 
-App::Cmd also gets us Getopt::Long::Descriptive.
+=pkgroup Tools for Working with the CPAN
 
 =pkg CPAN 1.87 support for sudo on install
 
@@ -33,6 +26,52 @@ App::Cmd also gets us Getopt::Long::Descriptive.
 
 =pkg CPAN::Uploader
 
+=pkg pip 0.12 supports installing from http URIs
+
+
+
+=pkgroup Tools for Building CPAN Distributions
+
+=pkg Dist::Zilla 1.093290 newest available
+
+=pkg Dist::Zilla::PluginBundle::RJBS 0.093200 newest available
+
+=pkg Module::CoreList 2.18 has info on 5.10.1
+
+=pkg Module::Install 0.74
+
+=pkg Perl::Critic 1.080
+
+=pkg Perl::Critic::Lax 0.007
+
+=pkg Perl::Tidy 20071205 supports 5.10
+
+=pkg Pod::Weaver 3.093130 latest available
+
+
+
+=pkgroup Application Frameworks
+
+=pkg App::Cmd 0.303 App::Cmd::Simple; compat with Getopt::Long::Descripive 0.081
+
+App::Cmd also gets us Getopt::Long::Descriptive.
+
+
+
+=pkgroup Email-Handling Libraries
+
+=pkg Email::Filter 1.02 
+
+=pkg Email::MIME 1.900 merged in Creator and Modifier modules
+
+=pkg Email::Sender
+
+=pkg Email::Sender::Transport::SQLite
+
+
+
+=pkgroup Other Libraries I Use
+
 =pkg Config::INI 0.011
 
 =pkg DBD::SQLite
@@ -40,14 +79,6 @@ App::Cmd also gets us Getopt::Long::Descriptive.
 =pkg Data::GUID 0.044 requires a new enough Data::UUID to work around Debian
 
 =pkg DateTime 0.51 provides CLDR support with fewest known bugs
-
-=pkg Dist::Zilla 1.093290 newest available
-
-=pkg Dist::Zilla::PluginBundle::RJBS 0.093200 newest available
-
-=pkg Email::Filter 1.02 
-
-=pkg Email::MIME 1.900 merged in Creator and Modifier modules
 
 =pkg Exception::Class 1.23 provides ->caught
 
@@ -61,23 +92,15 @@ App::Cmd also gets us Getopt::Long::Descriptive.
 
 =pkg Mixin::ExtraFields::Param 0.011
 
-=pkg Module::CoreList 2.18 has info on 5.10.1
-
-=pkg Module::Install 0.74
-
 =pkg Moose 0.74 supports duck_type
+
+=pkg Moose::Autobox 0.10 has "each" iterators
+
+=pkg namespace::autoclean
 
 =pkg PPI 1.201
 
 =pkg Params::Util 0.23 has working _INVOCANT
-
-=pkg Perl::Critic 1.080
-
-=pkg Perl::Critic::Lax 0.007
-
-=pkg Perl::Tidy 20071205 supports 5.10
-
-=pkg Pod::Weaver 3.093130 latest available
 
 =pkg Scalar::Util 1.18
 
@@ -86,8 +109,6 @@ App::Cmd also gets us Getopt::Long::Descriptive.
 =pkg Text::Markdown 1.0.24 has trust_list_start
 
 =pkg YAML::XS undef 
-
-=pkg pip 0.12 supports installing from http URIs
 
 =head1 PENDING INCLUSION:
 
