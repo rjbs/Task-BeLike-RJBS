718d68397c829ef16532d4233b15f6ec7bf6d76d
Ricardo Signes <rjbs@cpan.org>
2015-04-23 08:28:20 -0400

    tweaks to rjbs.pm

--- lib/rjbs.pm
+++ lib/rjbs.pm
@@ -1,16 +1,30 @@
 package rjbs;
+# ABSTRACT: all the junk that rjbs likes in his one-offs
 
-use 5.20.0;
+use 5.21.10;
 use feature ();
 use experimental ();
+
+=head1 OVERVIEW
+
+When you C<use rjbs> you get a whole bunch of other pragmata turned on.  It
+turns on strict, warnings, all the v5.20 features, signatures, postfix
+dereferencing, lexical subs, unambiguous bitwise operators, and and reference
+aliasing.
+
+The exact behavior of this module is subject to change.  Consider it the "toy
+inside" Task::BeLike::RJBS.
+
+=cut
 
 sub import {
   strict->import;
   warnings->import;
   feature->import(':5.20');
-  experimental->import(qw(signatures postderef lexical_subs));
-
-  # 5.22 - bitwise, refaliasing
+  experimental->import(qw(
+    signatures postderef lexical_subs
+    bitwise refaliasing
+  ));
 }
 
 1;
